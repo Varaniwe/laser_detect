43787daf1ef5b91c425a409d1af4c6fb88977823
varaniwe <ivarankin@yandex.ru>
2020-03-07 22:10:48 +0500

    master: CMake fix for Ffmpeg

--- prepare.cmd
+++ prepare.cmd
@@ -3,12 +3,11 @@ rmdir /S /Q build
 @set script_dir=%~dp0
 
 @set arch=i386
-@set CMAKE_GENERATOR=Visual Studio 15 2017
 
 @mkdir %script_dir%\build 2>nul 1>nul
 @set build_dir=%script_dir%\build\%arch%
 @mkdir %build_dir% 2>nul 1>nul
 @pushd %build_dir%
 shift
-cmake -WSno-dev -G "%CMAKE_GENERATOR%" %* ..\..
+cmake -WSno-dev %* ..\..
 @popd
